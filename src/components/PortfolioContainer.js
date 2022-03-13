d855e59ab44765d630dfb4347a1d82a7b659592c
Sabriel Gee <sabrielgee@Sabriels-iMac.local>
2022-03-13 10:47:41 -0600

    initial set up

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import About from './pages/About';
+import Work from './pages/Work';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // TODO: Add a comment describing the functionality of this method
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Work') {
+      return <Work />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* // TODO: Add a comment describing what we are passing as props */}
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* // TODO: Add a comment explaining what is happening on the following line */}
+      {renderPage()}
+    </div>
+  );
+}
