e538aa8ec3345d156944ef8248107831cfb23f3c
Sabriel Gee <sabrielgee@sabriels-imac.lan>
2022-03-15 18:57:59 -0600

    contact form stuff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-// import logo from './logo.svg';
 import React from "react";
 import './App.css';
 import PortfolioContainer from "./components/PortfolioContainer";
-import Form from './components/Form/index';
+
 
 const App = () => <PortfolioContainer />;
-
-// function handleFormSubmit() {
-//     return <Form />;
-// }
 
 export default App;
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Form from '../Form';
 
 export default function Contact() {
   return (
