b2028264b8398ea4288b9e5438b021b79a36fa1e
Sabriel Gee <sabrielgee@sabriels-imac.lan>
2022-03-22 18:53:42 -0600

    deployment and css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,21 @@
 import React from "react";
-// import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-// import {faGithub, faLinkedin, faInstagram} from "@fortawesome/free-brand-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGithub, faLinkedin, faInstagram} from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
     <footer>
       Made with intention by Sabriel Gee
       <br/>
-        {/* <a href="https://github.com/sabrielg">
+        <a className="px-2" href="https://github.com/sabrielg">
             <FontAwesomeIcon icon={faGithub}/>
         </a>
-        <a href="https://www.linkedin.com/in/sabriel-gee/">
+        <a className="px-2" href="https://www.linkedin.com/in/sabriel-gee/">
         <FontAwesomeIcon icon={faLinkedin}/>
         </a>
-        <a href="https://www.instagram.com/sabrieltoothtiger/">
+        <a className="px-2" href="https://www.instagram.com/sabrieltoothtiger/">
         <FontAwesomeIcon icon={faInstagram}/>
-        </a> */}
+        </a>
         <br/>
       &copy; 2022 Sabriel Gee, llc
     </footer>
